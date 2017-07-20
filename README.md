72a74b8279fa4208b8ca02ff308a69e391a05b87
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-20 15:27:39 +0500

    Use min and max functions

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ $ python bars.py <path to file>
 
 You will see for example:
 
-Enter Latitude: 55.25
-Enter Longitude: 46.55
-The smallest bar is  ДИОРАМА БАР Кутузовский проспект, дом 2/1, корпус 1А seats: 3
+Enter Latitude: 55.8274371
+Enter Longitude: 37.49260332
+The nearest bar is  Альтернатива 2 Ленинградское шоссе, дом 22 seats: 40
+The smallest bar is  БАР. СОКИ Дубравная улица, дом 34/29 seats: 0
 The biggest bar is  Спорт бар «Красная машина» Автозаводская улица, дом 23, строение 1 seats: 450
-The nearest bar is  Таверна проспект Защитников Москвы, дом 8 seats: 16
 Have a nice evening!
 ```
 
--- bars.py
+++ bars.py
@@ -11,29 +11,14 @@ def load_bar_information(filepath):
 
 
 def get_biggest_bar(bar_information):
-    seats_count = {}
-    for bars in bar_information:
-        seats_count[bars["global_id"]] = bars["SeatsCount"]
-    return sorted(seats_count, key=lambda x: int(seats_count[x]), reverse=True)[0]
+    return max((bar['SeatsCount'], bar['global_id']) for bar in bar_information)
 
 
 def get_smallest_bar(bar_information):
-    seats_count = {}
-    for bars in bar_information:
-        if bars["SeatsCount"] > 2:
-            seats_count[bars["global_id"]] = bars["SeatsCount"]
-
-    return sorted(seats_count, key=lambda x: int(seats_count[x]), reverse=False)[0]
-
-
+    return min((bar['SeatsCount'], bar['global_id']) for bar in bar_information)
+    
 def get_closest_bar(bar_information, longitude, latitude):
-
-    distance_count = {}
-    for bars in bar_information:
-        distance_count[bars["global_id"]] = distance(longitude, latitude, coordx2=float(
-            bars["Longitude_WGS84"]), coordy2=float(bars["Latitude_WGS84"]))
-    return sorted(distance_count, key=lambda x: float(distance_count[x]), reverse=False)[0]
-
+    return min((distance(longitude, latitude, coordx2=float(bar["Longitude_WGS84"]), coordy2=float(bar["Latitude_WGS84"])), bar['global_id']) for bar in bar_information)
 
 def distance(coordx1, coordy1, coordx2, coordy2):
     return ((coordx2 - coordx1)**2 + (coordy2 - coordy1)**2)**0.5
@@ -42,15 +27,15 @@ def distance(coordx1, coordy1, coordx2, coordy2):
 def print_results(filepath):
     bar_information = load_bar_information(filepath)
     for bars in bar_information:
-        if bars["global_id"] == get_biggest_bar(bar_information):
+        if bars["global_id"] == get_biggest_bar(bar_information)[1]:
             print('The biggest bar is ',
                   bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
 
-        if bars["global_id"] == get_smallest_bar(bar_information):
+        if bars["global_id"] == get_smallest_bar(bar_information)[1]:
             print('The smallest bar is ',
                   bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
 
-        if bars["global_id"] == get_closest_bar(bar_information, your_longitude, your_latitude):
+        if bars["global_id"] == get_closest_bar(bar_information, your_longitude, your_latitude)[1]:
             print('The nearest bar is ',
                   bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
     print('Have a nice evening!')
