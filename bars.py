ee34ae8c1c7d06a73b30589c1ac2f3f236e3a0b8
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-21 11:19:50 +0500

    Fixed by TODO from 20.07

--- bars.py
+++ bars.py
@@ -11,46 +11,50 @@ def load_bar_information(filepath):
 
 
 def get_biggest_bar(bar_information):
-    return max((bars['SeatsCount'], bars['global_id']) for bars in bar_information)
+    return max(bar_information, key=lambda x: x["SeatsCount"])
 
 
 def get_smallest_bar(bar_information):
-    return min((bars['SeatsCount'], bars['global_id']) for bars in bar_information)
+    return min(bar_information, key=lambda x: x["SeatsCount"])
 
 
 def get_closest_bar(bar_information, longitude, latitude):
-    return min((distance(longitude, latitude, coordx2=float(bars["Longitude_WGS84"]), coordy2=float(bars["Latitude_WGS84"])), bars['global_id']) for bars in bar_information)
+    return min(bar_information, key=lambda x: distance(longitude, latitude, coordx2=float(x["Longitude_WGS84"]), coordy2=float(x["Latitude_WGS84"])))
 
 
 def distance(coordx1, coordy1, coordx2, coordy2):
     return ((coordx2 - coordx1)**2 + (coordy2 - coordy1)**2)**0.5
 
 
-def print_results(filepath):
-    bar_information = load_bar_information(filepath)
-    for bars in bar_information:
-        if bars["global_id"] == get_biggest_bar(bar_information)[1]:
-            print('The biggest bar is ',
-                  bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
-
-        if bars["global_id"] == get_smallest_bar(bar_information)[1]:
-            print('The smallest bar is ',
-                  bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
-
-        if bars["global_id"] == get_closest_bar(bar_information, your_longitude, your_latitude)[1]:
-            print('The nearest bar is ',
-                  bars["Name"], bars["Address"], 'seats:', bars["SeatsCount"])
-    print('Have a nice evening!')
+def get_info(bar_json):
+    print(bar_json["Name"], bar_json["Address"],
+          'seats:', bar_json["SeatsCount"])
+    pass
 
 
 if __name__ == '__main__':
     jsonfile = sys.argv[1]
-    try:
-        your_latitude = float(input('Enter Latitude: '))
-        your_longitude = float(input('Enter Longitude: '))
-    except ValueError:
-        your_latitude = None
-        your_longitude = None
-    if (your_latitude or your_longitude) is None:
-        print('Error! Check your input!')
-    print_results(jsonfile)
+    if os.path.isfile(jsonfile):
+        try:
+            your_latitude = float(input('Enter Latitude: '))
+            your_longitude = float(input('Enter Longitude: '))
+        except ValueError:
+            your_latitude = None
+            your_longitude = None
+            if (your_latitude or your_longitude) is None:
+                print('Error! Check your input!')
+        bar_info = load_bar_information(jsonfile)
+        biggest = get_biggest_bar(bar_info)
+        smallest = get_smallest_bar(bar_info)
+        closest = get_closest_bar(bar_info, your_longitude, your_latitude)
+
+        print('The biggest is')
+        get_info(biggest)
+        print('The smallest is')
+        get_info(smallest)
+        print('The closest is')
+        get_info(closest)
+        
+    else:        
+        print("Error. Check your JSON file!")
+
