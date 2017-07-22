9169faa43a656205453b24af1e3b6e7a12734cda
maksart72 <maksim.artemyev@gmail.com>
2017-07-22 20:07:18 +0500

    Correct some mistakes from 21/07

--- bars.py
+++ bars.py
@@ -19,42 +19,39 @@ def get_smallest_bar(bar_information):
 
 
 def get_closest_bar(bar_information, longitude, latitude):
-    return min(bar_information, key=lambda x: distance(longitude, latitude, coordx2=float(x["Longitude_WGS84"]), coordy2=float(x["Latitude_WGS84"])))
+    return min(bar_information, key=lambda x: distance(longitude, latitude, coordx2=float(x["Longitude_WGS84"]), coordy2=float(x
+["Latitude_WGS84"])))
 
 
 def distance(coordx1, coordy1, coordx2, coordy2):
     return ((coordx2 - coordx1)**2 + (coordy2 - coordy1)**2)**0.5
 
 
-def get_info(bar_json):
+def get_bar_information(bar_json):
     print(bar_json["Name"], bar_json["Address"],
           'seats:', bar_json["SeatsCount"])
-    pass
 
 
 if __name__ == '__main__':
     jsonfile = sys.argv[1]
-    if os.path.isfile(jsonfile):
+    if not os.path.exists(jsonfile):
+        print("Error! Check your JSON file!")
+    else:
         try:
+            jsonfile = sys.argv[1]
+            os.path.exists(jsonfile)
             your_latitude = float(input('Enter Latitude: '))
             your_longitude = float(input('Enter Longitude: '))
         except ValueError:
-            your_latitude = None
-            your_longitude = None
-            if (your_latitude or your_longitude) is None:
-                print('Error! Check your input!')
-        bar_info = load_bar_information(jsonfile)
-        biggest = get_biggest_bar(bar_info)
-        smallest = get_smallest_bar(bar_info)
-        closest = get_closest_bar(bar_info, your_longitude, your_latitude)
-
-        print('The biggest is')
-        get_info(biggest)
-        print('The smallest is')
-        get_info(smallest)
-        print('The closest is')
-        get_info(closest)
-        
-    else:        
-        print("Error. Check your JSON file!")
-
+            print('Error! Check your geo position input!')
+        else:
+            bar_info = load_bar_information(jsonfile)
+            biggest = get_biggest_bar(bar_info)
+            smallest = get_smallest_bar(bar_info)
+            closest = get_closest_bar(bar_info, your_longitude, your_latitude)
+            print('The biggest is')
+            get_bar_information(biggest)
+            print('The smallest is')
+            get_bar_information(smallest)
+            print('The closest is')
+            get_bar_information(closest)
