1aaa25d273c3ba5a3498e322ab8d7f1b9fe3d3b9
maksart72 <maksim.artemyev@gmail.com>
2017-07-23 18:36:45 +0500

    Add exception for argument, check JSON file format,

--- bars.py
+++ bars.py
@@ -4,11 +4,11 @@ import sys
 
 
 def load_bar_information(filepath):
-    if not os.path.exists(filepath):
-        return None
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
-
+    try:
+        with open(filepath, 'r') as file_handler:
+            return json.load(file_handler)
+    except json.JSONDecodeError:
+        raise Exception("Error! This is not JSON format file!")
 
 def get_biggest_bar(bar_information):
     return max(bar_information, key=lambda x: x["SeatsCount"])
@@ -27,31 +27,30 @@ def distance(coordx1, coordy1, coordx2, coordy2):
     return ((coordx2 - coordx1)**2 + (coordy2 - coordy1)**2)**0.5
 
 
-def get_bar_information(bar_json):
+def print_bar_information(bar_json):
     print(bar_json["Name"], bar_json["Address"],
           'seats:', bar_json["SeatsCount"])
 
 
 if __name__ == '__main__':
-    jsonfile = sys.argv[1]
-    if not os.path.exists(jsonfile):
-        print("Error! Check your JSON file!")
+    try:
+        jsonfile = sys.argv[1]
+        if not os.path.exists(jsonfile):
+            raise Exception("Error! File doesn't exist!")
+        your_latitude = float(input('Enter Latitude: '))
+        your_longitude = float(input('Enter Longitude: '))
+    except IndexError:
+        print("Error: Empty argument, try bars.py <filename>")
+    except ValueError:
+        print('Error! Check your geo position input!')
     else:
-        try:
-            jsonfile = sys.argv[1]
-            os.path.exists(jsonfile)
-            your_latitude = float(input('Enter Latitude: '))
-            your_longitude = float(input('Enter Longitude: '))
-        except ValueError:
-            print('Error! Check your geo position input!')
-        else:
-            bar_info = load_bar_information(jsonfile)
-            biggest = get_biggest_bar(bar_info)
-            smallest = get_smallest_bar(bar_info)
-            closest = get_closest_bar(bar_info, your_longitude, your_latitude)
-            print('The biggest is')
-            get_bar_information(biggest)
-            print('The smallest is')
-            get_bar_information(smallest)
-            print('The closest is')
-            get_bar_information(closest)
+        bar_info = load_bar_information(jsonfile)
+        biggest = get_biggest_bar(bar_info)
+        smallest = get_smallest_bar(bar_info)
+        closest = get_closest_bar(bar_info, your_longitude, your_latitude)
+        print('The biggest is')
+        print_bar_information(biggest)
+        print('The smallest is')
+        print_bar_information(smallest)
+        print('The closest is')
+        print_bar_information(closest)
